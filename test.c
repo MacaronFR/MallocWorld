bc1e8ab94f48637b3066452e45f8c610284029ed
Barlords <basile.pulin@gmail.com>
2021-10-11 23:33:42 +0200

    suppression des tests

--- test.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by basil on 11/10/2021.
-//
-
-#include "test.h"
--- test.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by basil on 11/10/2021.
-//
-
-#ifndef MALLOCWORLD_TEST_H
-#define MALLOCWORLD_TEST_H
-
-#endif //MALLOCWORLD_TEST_H
