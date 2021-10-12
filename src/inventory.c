872b3fe7521be99e4b2a8afa2b9e3ba364ccf17e
MacaronFR <denisft77@gmail.com>
2021-10-12 19:54:29 +0200

    change in inventory.c

--- src/inventory.c
+++ src/inventory.c
@@ -1,101 +1,94 @@
-//
-// Created by basil on 12/10/2021.
-//
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
-typedef int bool ;  //définition du type booléen
-#define false  0;
-#define true  1;
+typedef struct s_item{
+	int8_t durability; //durabilité ou cout en durabilité
+	uint16_t type; // type de l'objet (ressources/item arme/armure/outil/potion/arbre/roche/plante épée/lance/…
+	int32_t id; //id objet
+	int32_t *craft; // si item son craft en tableau d'id nécessaire
+	uint8_t flag; // zone
+} item;
 
+typedef struct s_slot{
+	item **item;
+	int quantity;
+} slot;
 
-typedef struct Item {
-    int object;
-    int durability;
-}Item;
+typedef struct s_inventory{
+	slot *slots[10];
+} inventory;
 
-typedef struct Slot {
-    Item* item;
-    int quantity;
-}Slot;
-
-typedef struct Inventory {
-    Slot* slots[10];
-}Inventory;
-
-//CONSTANTE
-const int MAX_SLOTS_INVENTORY = 10;
+#define MAX_SLOTS_INVENTORY 10
 
 //---------------------- Creation et Destruction ----------------------
-void initInventory(Inventory* inventory) {
-    for(int i=0 ; i<MAX_SLOTS_INVENTORY ; i++) {
-        inventory->slots[i]->item = NULL;
-        inventory->slots[i]->quantity = 0;
-    }
+void initInventory(inventory *inventory){
+	for(int i = 0; i < MAX_SLOTS_INVENTORY; i++){
+		inventory->slots[i]->item = NULL;
+		inventory->slots[i]->quantity = 0;
+	}
 }
 
-Inventory* createInventory() {
-    Inventory* inventory = malloc(sizeof(Inventory));
-    initInventory(inventory);
+inventory *createInventory(){
+	inventory *inventory = malloc(sizeof(inventory));
+	initInventory(inventory);
 }
 
-void freeInventory(Inventory* inventory) {
-    for (int i=0; i<MAX_SLOTS_INVENTORY ; i++) {
-        free(inventory->slots[i]);
-    }
-    free(inventory);
+void freeInventory(inventory *inventory){
+	for(int i = 0; i < MAX_SLOTS_INVENTORY; i++){
+		free(inventory->slots[i]);
+	}
+	free(inventory);
 }
 
 //---------------------- Test du contenue ----------------------
-bool isLikeItem(Item* item1, Item* item2) {
-    return item1->object == item2->object;
+bool isLikeItem(item *item1, item *item2){
+	return item1->id == item2->id;
 }
 
-bool isFullStack(Slot* slot) {
-    return slot->quantity >= 20;
+bool isFullStack(slot *slot){
+	return slot->quantity >= 20;
 }
 
-bool isFullInventory(Inventory* inventory) {
-    for(int i=0 ; i<MAX_SLOTS_INVENTORY ; i++) {
-        if(inventory->slots[i]->quantity == 0)
-            return false;
-    }
-    return true;
+bool isFullInventory(inventory *inventory){
+	for(int i = 0; i < MAX_SLOTS_INVENTORY; i++){
+		if(inventory->slots[i]->quantity == 0)
+			return false;
+	}
+	return true;
 }
 
-bool isFullStackInventory(Inventory* inventory) {
-    if(!isFullInventory(inventory)) {
-        return false;
-    }
-    else{
-        for(int i=0 ; i<MAX_SLOTS_INVENTORY ; i++) {
-            if(inventory->slots[i]->item == 0)
-                return false;
-        }
-    }
-
+bool isFullStackInventory(inventory *inventory){
+	if(!isFullInventory(inventory)){
+		return false;
+	}
+	for(int i = 0; i < MAX_SLOTS_INVENTORY; i++){
+		if(inventory->slots[i]->item == 0)
+			return false;
+	}
+	return true;
 }
 
-bool isInInventory(Inventory* inventory, Item* item) {
-    int i = 0;
-    while(inventory->slots[i] != NULL && i<MAX_SLOTS_INVENTORY) {
-        if(isLikeItem(inventory->slots[i]->item, item)) {
-            return true;
-        }
-        i++;
-    }
-    return false;
+bool isInInventory(inventory *inventory, item *item){
+	int i = 0;
+	while(inventory->slots[i] != NULL && i < MAX_SLOTS_INVENTORY){
+		if(isLikeItem(inventory->slots[i]->item, item)){
+			return true;
+		}
+		i++;
+	}
+	return false;
 }
 
 //---------------------- Récupération et Modification ----------------------
-int indexInInventory(Inventory* inventory, Item* item) {
-    int i = 0;
-    while(inventory->slots[i] != NULL && i<MAX_SLOTS_INVENTORY) {
-        if(isLikeItem(inventory->slots[i]->item, item)) {
-            return i;
-        }
-        i++;
-    }
-    return -1;
+int indexInInventory(inventory *inventory, item *item){
+	int i = 0;
+	while(inventory->slots[i] != NULL && i < MAX_SLOTS_INVENTORY){
+		if(isLikeItem(inventory->slots[i]->item, item)){
+			return i;
+		}
+		i++;
+	}
+	return -1;
 }
