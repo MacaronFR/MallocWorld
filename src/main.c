55e566933af18dc21cea24acd251803b7b11acac
Barlords <basile.pulin@gmail.com>
2021-11-13 10:45:44 +0100

    modif des tests

--- src/main.c
+++ src/main.c
@@ -23,46 +23,7 @@ void printPerlin(int res, FILE *f) {
 	fprintf(f, "%2d", res);
 }
 
-int testMacaron(int argc, char **argv) {
-	/*item *res = load_item("../test.mw");
-	if(res == NULL){
-		return 1;
-	}
-	free(res);*/
-	size_t nItem;
-	item **itemList = load_items("..", &nItem);
-	if (itemList != NULL) {
-		if (checkCraftValidity(itemList, nItem)) {
-			printf("OK");
-		} else {
-			printf("NIKK");
-		}
-		freeItemList(itemList, nItem);
-	}
-	/*
-	FILE *f = fopen("/home/macaron/Documents/res.csv", "w");
-	int ***map = generateMap(atoi(argv[1]));
-	for(int k = 0; k < 3; ++k){
-		for(int i = 0; i < 100; ++i){
-			for(int j = 0; j < 100; ++j){
-				fprintf(f, "%2d", map[k][i][j]);
-				if(j != 99) fprintf(f, ", ");
-			}
-			fprintf(f, "\n");
-		}
-		for(int i = 0; i < 100; i++){
-			fprintf(f, "-1");
-			if(i != 99) fprintf(f, ", ");
-		}
-		fprintf(f, "\n");
-	}
-	freeMap(map, 3, 100);
-	fclose(f);
-	return 0;*/
-	/*monster *tmp = loadMonster("../monster.mw");
-	deleteMonster(tmp);
-	return 0;*/
-}
+
 
 void testPlayer(player *player) {
 	printFlag(0, "PLAYER");
@@ -101,6 +62,32 @@ void testItem() {
 	printFlag(1, "ITEM");
 }
 
+int testMacaron(int argc, char **argv) {
+	testItem();
+	/*
+	FILE *f = fopen("/home/macaron/Documents/res.csv", "w");
+	int ***map = generateMap(atoi(argv[1]));
+	for(int k = 0; k < 3; ++k){
+		for(int i = 0; i < 100; ++i){
+			for(int j = 0; j < 100; ++j){
+				fprintf(f, "%2d", map[k][i][j]);
+				if(j != 99) fprintf(f, ", ");
+			}
+			fprintf(f, "\n");
+		}
+		for(int i = 0; i < 100; i++){
+			fprintf(f, "-1");
+			if(i != 99) fprintf(f, ", ");
+		}
+		fprintf(f, "\n");
+	}
+	freeMap(map, 3, 100);
+	fclose(f);
+	return 0;*/
+	/*monster *tmp = loadMonster("../monster.mw");
+	deleteMonster(tmp);
+	return 0;*/
+}
 
 int main(int argc, char **argv) {
 	player *player = createPlayer();
